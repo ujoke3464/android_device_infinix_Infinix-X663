18c906b1b7a9f33c98106828fc410f54c1afe7f2
ujoke3464 <109740294+ujoke3464@users.noreply.github.com>
2023-06-29 22:27:09 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,9 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Remove these lines:
+# add_lunch_combo omni_Infinix-X663-userdebug
+# add_lunch_combo omni_Infinix-X663-eng
+# add_lunch_combo omni_Infinix-X663-user
 
-add_lunch_combo omni_Infinix-X663-user
-add_lunch_combo omni_Infinix-X663-userdebug
-add_lunch_combo omni_Infinix-X663-eng
+# Add these lines instead:
+COMMON_LUNCH_CHOICES += omni_Infinix-X663-userdebug
+COMMON_LUNCH_CHOICES += omni_Infinix-X663-eng
+COMMON_LUNCH_CHOICES += omni_Infinix-X663-user
